42ec915262ddc2afadb3410d152bd7396d4ef7df
Atul <atulmunawaroh77@gmail.com>
2018-07-17 15:52:32 +0700

    header, dashboard

--- application/views/base/header.php
+++ application/views/base/header.php
@@ -24,34 +24,15 @@
             <ul class="dropdown-menu">
               <!-- User image -->
               <li class="user-header">
-                <img src="<?php echo base_url('assets/adminlte'); ?>/dist/img/user2-160x160.jpg" class="img-circle" alt="User Image">
+                <i class="ion-person" style="font-size: 75px;"></i>
                 <p>
                   Alexander Pierce - Web Developer
-                  <small>Member since Nov. 2012</small>
                 </p>
-              </li>
-              <!-- Menu Body -->
-              <li class="user-body">
-                <div class="row">
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Followers</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Sales</a>
-                  </div>
-                  <div class="col-xs-4 text-center">
-                    <a href="#">Friends</a>
-                  </div>
-                </div>
-                <!-- /.row -->
               </li>
               <!-- Menu Footer-->
               <li class="user-footer">
-                <div class="pull-left">
-                  <a href="#" class="btn btn-default btn-flat">Profile</a>
-                </div>
                 <div class="pull-right">
-                  <a href="#" class="btn btn-default btn-flat">Sign out</a>
+                  <a href="<?php echo base_url('auth/logout');?>" class="btn btn-default btn-flat">Sign out</a>
                 </div>
               </li>
             </ul>
